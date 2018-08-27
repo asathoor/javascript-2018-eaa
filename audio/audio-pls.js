6ba623b1b785a1fd0e66a52e13c0416cfe98ad2a
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2018-08-27 22:20:48 +0200

    audio and pls

--- audio/audio-pls.js
+++ audio/audio-pls.js
@@ -1,17 +1,27 @@
 /**
- * file: audio.js
+ * file: audio-pls.js
+ * purpose: play internet radio streams from SomaFM
  **/
+
+// random track no.
+let Rnd = function() {
+  return Math.round(
+    Math.random() * myTracks.length
+  );
+}
 
 // sample track list
 let myTracks = [
   'http://ice3.somafm.com/missioncontrol-128-mp3',
-  'more tracks here ...'
+  'http://ice1.somafm.com/thistle-128-mp3'
 ]
 
-/*
- SOMAFM
- Use the link found in the pls files.
- */
+myMusic.src = myTracks[Rnd()]; // random track from the track list
+
 myMusic.controls = true;
-myMusic.src = myTracks[0];
 myMusic.currentTime = true;
+console.log('Playing: ' + myMusic.src);
+
+// track list
+
+// let your user chage the soundscape by a click ...
