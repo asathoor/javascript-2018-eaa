757bfd1d74f08ab0a3f371757a5f93cf90dc6f58
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2018-08-29 16:32:22 +0200

    undervisning dd.

--- audio/audio.js
+++ audio/audio.js
@@ -1,25 +1,13 @@
-/**
- * file: audio.js
- **/
-let myTracks = [
-  'workoutmix.mp3',
-  'yoursample.mp3',
-  'etc.'
-]
+  /**
+   * file: audio.js
+   **/
 
-/*
- Sample settings below.
- More @url: https://www.w3schools.com/tags/ref_av_dom.asp
- */
-myMusic.controls = true;
-myMusic.src = myTracks[0];
-//myMusic.play(); // ... yeah, but is it wise?
-myMusic.currentTime = true;
-//myMusic.style.height = '75px';
-
-// Exercise A)
-// Create a function that will play a random mp3 from myTracks
-// let the functions initiate automaticly
-
-// Exercise B)
-// Play MP3s according to the user's will
+  /*
+   Sample settings below.
+   More @url: https://www.w3schools.com/tags/ref_av_dom.asp
+   */
+  myMusic.controls = true;
+  myMusic.src = 'http://ice1.somafm.com/suburbsofgoa-128-mp3';
+  //myMusic.play(); // ... yeah, but is it wise?
+  myMusic.currentTime = true;
+  myMusic.style.height = '75px';
---
+++ ups.js
@@ -0,0 +1,20 @@
+var huskAtFotografereGruppemedlemmer = false;
+
+if (huskAtFotografereGruppemedlemmer == false){
+  alert('Skriv navne og kontaktinfo ... og giv sedlen til Per.');
+}
+
+var hobbit = {
+  key:'value',
+  navn:'Bilbo Baggins',
+  adresse: 'Shire',
+  livret: 'Cake',
+  alder: 111,
+  ryger: true,
+  rygeInstrument: 'pibe',
+  etAarMere: function(){
+    return alder++;
+  }
+}
+
+document.write( hobbit.ryger );
