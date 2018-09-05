a1927d5ffdb9131b3c9c46ebe57557b98782c001
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2018-09-05 15:27:10 +0200

    3d buildings

--- maps/aarhusDok1.js
+++ maps/aarhusDok1.js
@@ -6,7 +6,49 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhdGhvb3IiLCJhIjoiY2oyd3hlbzU3MDA5NzJxbm9iM
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/asathoor/cjfkvmngz9bz52rpdgbs29r0w',
-  center: [10.150040, 56.117220],
-  zoom: 14.0,
-  pitch: 50
+  center: [10.213378, 56.153169],
+  zoom: 16.2,
+  pitch: 72,
+  bearing: 22
+});
+
+// The 'building' layer in the mapbox-streets vector source contains building-height
+// data from OpenStreetMap.
+map.on('load', function() {
+    // Insert the layer beneath any symbol layer.
+    var layers = map.getStyle().layers;
+
+    var labelLayerId;
+    for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+        }
+    }
+
+    map.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+            'fill-extrusion-color': '#aaa',
+
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            'fill-extrusion-height': [
+                "interpolate", ["linear"], ["zoom"],
+                15, 0,
+                15.05, ["get", "height"]
+            ],
+            'fill-extrusion-base': [
+                "interpolate", ["linear"], ["zoom"],
+                15, 0,
+                15.05, ["get", "min_height"]
+            ],
+            'fill-extrusion-opacity': .6
+        }
+    }, labelLayerId);
 });
